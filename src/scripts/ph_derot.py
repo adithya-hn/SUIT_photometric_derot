5d0349619d85e5eef542edee1a45934dd4113488
Adithya HN <76843230+adithya-hn@users.noreply.github.com>
2025-09-01 12:45:32 +0530

    Update ph_derot.py

--- src/scripts/ph_derot.py
+++ src/scripts/ph_derot.py
@@ -1,4 +1,14 @@
 
+'''
+Created on: 
+@author: adithya-hn
+Aim: Derotate the SUIT/Aditya-L1 images
+Method: We make a subpixel binning for each pixel and then apply the rotation, and then bin by the same amount 
+to convert them back to their original size
+
+
+'''
+
 import sunpy.map
 import matplotlib.pyplot as plt
 import astropy.units as u
@@ -6,7 +16,7 @@ import numpy as np
 
 
 
-def get_rotMap(image_map,rot_angle):
+def get_rotMap(image_map,rot_angle): #apply rotation 
     old_header=image_map.fits_header
     o_sum=np.sum(np.array(image_map.data,dtype='int64'))
     rot_map=image_map.rotate(angle=rot_angle * u.deg)
@@ -16,7 +26,7 @@ def get_rotMap(image_map,rot_angle):
     old_header['CRPIX1']=rotMap_header['CRPIX1']
     old_header['CRPIX2']=rotMap_header['CRPIX2']
     err=(o_sum-r_sum)*100/o_sum
-    print(o_sum,r_sum,err)
+    #print(o_sum,r_sum,err)
     Rotated_map=sunpy.map.Map(rot_map.data,old_header)
     return Rotated_map,err
 
@@ -28,7 +38,7 @@ def create_subpixels(matrix, scale):
   new_matrix=new_matrix/pow(scale,2)
   return new_matrix
 
-def bin_back(matrix,scale):
+def bin_back(matrix,scale): #bin back or create a super pixel to have the same size.
    new_matrix = np.zeros((int(matrix.shape[0] / scale), int(matrix.shape[1] / scale)))
    for i in range(new_matrix.shape[0]):
     for j in range(new_matrix.shape[1]):
@@ -50,12 +60,13 @@ def get_photometric_derot(input_map,rot_angle,bin_scale):
     Rotated_map.save(f'Rotated_Map_{rot_angle}deg.fits',overwrite=True)
     return Rotated_map,err
 
-input_file="/Analysis/Research_Projects/SUIT_work/derotation/data/raw/SUT_T24_0956_000465_Lev1.0_2024-07-10T13.05.51.135_0983NB03.fits"
-input_map = sunpy.map.Map(input_file)
-bin_scale=4
-angle=7 # anticlock direction
-Rotated_map,err=get_photometric_derot(input_map,angle,bin_scale)
-print(f"Error in rotation: {err}")
-Rotated_map.save(f'Rotated_Map_{angle}deg.fits',overwrite=True)
+if __name__=='__main__':
+    input_file="/path/to/do/fits_image.fits"
+    input_map = sunpy.map.Map(input_file)
+    bin_scale=4 #each pixel will be made into 4 subpixels before rotation
+    angle=7     #anticlock direction
+    Rotated_map,err=get_photometric_derot(input_map,angle,bin_scale)
+    print(f"Error in rotation: {err}") #% error
+    Rotated_map.save(f'Rotated_Map_{angle}deg.fits',overwrite=True)
 
 
